228d702f474ffccc790981651d311044a42ddede
Shruthi <Shruthi1024@gmail.com>
2019-06-05 19:41:11 -0500

    addded bg

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
 import './App.css';
 import Viewer from './Viewer'
+import Blinker from './assets/Blinker.png';
+import Bg from './assets/bg.jpg';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Viewer />
+        {/* <Viewer /> */}
+        <img src={Bg} className='background'/>
+        <div className='menu__items'>
+          <div>
+              Shruthi Subramani
+            </div>
+            <div className='menu__item resume'>
+                <img src={Blinker} className='menu__blinker res' />
+                Resume
+            </div>
+            <div>
+                <img src={Blinker} className='menu__blinker ques' />
+                Quest Log
+            </div>
+            <div>
+                <img src={Blinker} className='menu__blinker sub' />
+                Subject Matrix
+            </div>
+            <div>
+                <img src={Blinker} className='menu__blinker cert' />
+                Certifications
+            </div>
+            <div>
+                <img src={Blinker} className='menu__blinker cont' />
+                Contact
+            </div>
+        </div>
       </div>
     );
   }
