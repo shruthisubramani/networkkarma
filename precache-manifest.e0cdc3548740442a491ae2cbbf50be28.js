76344625bab495696734496fa33fbb44e01e0fda
Shruthi <Shruthi1024@gmail.com>
2019-12-27 22:15:22 -0600

    Updates

---
+++ precache-manifest.e0cdc3548740442a491ae2cbbf50be28.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "465392e6bfd0b5affd96ef09d8a132a5",
+    "url": "/networkkarma/static/media/bg.465392e6.jpg"
+  },
+  {
+    "revision": "5254923c2e926d87597108ee2cf7a042",
+    "url": "/networkkarma/static/media/Blinker.5254923c.png"
+  },
+  {
+    "revision": "17fbc4d683401d836c02",
+    "url": "/networkkarma/static/js/runtime~main.02d47a24.js"
+  },
+  {
+    "revision": "52efd458a1265b561e9a",
+    "url": "/networkkarma/static/js/main.2989aca0.chunk.js"
+  },
+  {
+    "revision": "686e3fac581419a0d067",
+    "url": "/networkkarma/static/js/2.a4926d5e.chunk.js"
+  },
+  {
+    "revision": "52efd458a1265b561e9a",
+    "url": "/networkkarma/static/css/main.d96dd09d.chunk.css"
+  },
+  {
+    "revision": "686e3fac581419a0d067",
+    "url": "/networkkarma/static/css/2.d0562a88.chunk.css"
+  },
+  {
+    "revision": "4e97d75181b1e742dca883edab814d8c",
+    "url": "/networkkarma/index.html"
+  }
+];
